616f0fa132a4a4ef3aea3395ffef136ba9ee9221
Kedar Kiran Ghadyalji <kedarghadyalji@gmail.com>
2026-04-03 19:11:28 +0530

    Implement authentication and file upload middleware

---
+++ Backend/src/routes/auth.routes.js
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import * as authController from "../controllers/auth.controller.js";
+import * as authMiddleware from "../middlewares/auth.middleware.js";
+const authRouter = Router();
+
+/**
+ * @route POST /api/auth/register
+ * @description Register a new user
+ * @access Public
+ */
+authRouter.post("/register", authController.registerUserController);
+
+/**
+ * @route POST /api/auth/login
+ * @description login user with email and password
+ * @access Public
+ */
+authRouter.post("/login", authController.loginUserController);
+
+/**
+ * @route GET /api/auth/logout
+ * @description clear token from user cookie and add the token in blacklist
+ * @access public
+ */
+authRouter.get("/logout", authController.logoutUserController);
+
+/**
+ * @route GET /api/auth/get-me
+ * @description get the current logged in user details
+ * @access private
+ */
+authRouter.get(
+  "/get-me",
+  authMiddleware.authUser,
+  authController.getMeController,
+);
+
+export default authRouter;
